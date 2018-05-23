501d39e8a3b7ff06a1d483a0bfab1eb183181448
Charlie Cooksey <cdcooksey@gmail.com>
2018-05-23 06:06:58 -0600

    adds `Author#full_name` method and tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:author) { create(:author) }
+  describe 'FactoryBot' do
+    it { expect(author).to be_kind_of(Author) }
+  end
+  describe '#full_name' do
+    let(:name) { "#{author.first_name} #{author.last_name}" }
+    it { expect(author.full_name).to eq(name) }
+  end
 end
