60343cfea2a11457ad82c3f584807173a18972b5
Charlie Cooksey <cdcooksey@gmail.com>
2018-05-23 13:43:47 -0600

    adds integration test for `AuthorsController#index`

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,9 @@
 class AuthorsController < ApplicationController
+  def index
+    res = AuthorSerializer.new(Author.all)
+    render json: res.serializable_hash, status: :ok
+  end
+
+  def show
+  end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
 
+  let(:authors) { create_list(:author, 3) }
+  let(:actual) { JSON.parse(response.body) }
+
+  describe 'GET /authors' do
+    let(:expected_payload) { JSON.parse(AuthorSerializer.new(authors).serialized_json) }
+
+    before do
+      authors
+      get :index
+    end
+    it { expect(response).to have_http_status(200) }
+
+    it 'returns AuthorSerializer' do
+      expect(actual).to eq(expected_payload)
+    end
+  end
 end
